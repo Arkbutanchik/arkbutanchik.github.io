8dde745f165a53fb4a42f52c14edafed5cd226f8
Arkbutan <98645506+Arkbutanchik@users.noreply.github.com>
2024-07-10 10:52:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
-  autoRotate: true,
-  autoRotateSpeed: 0.3,
   controlBar: false,
 });
 
