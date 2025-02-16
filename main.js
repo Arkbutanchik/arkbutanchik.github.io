7fabbc701c8b42258343ffc96b4d60f86e43e636
Arkbutan <98645506+Arkbutanchik@users.noreply.github.com>
2025-02-16 13:29:06 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const panoramaImage = new PANOLENS.ImagePanorama("images/pan.png");
-const imageContainer = document.querySelector(".image-container");
-
-const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-  controlBar: false,
-});
-
-viewer.add(panoramaImage);
